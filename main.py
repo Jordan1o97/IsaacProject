1559c63b53ad4933302ed289da74bfa94fb92567
Isaac Francis <isaacfrancis@Isaacs-MacBook-Pro.local>
2021-05-29 16:50:02 -0300

    Attempted script, no resultant output

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
-print("Lets make a project :)")
-print("Isaac")
+# Basic attempt at establishing pytube; requires pytube
+
+from pytube import YouTube
+link = input("Please input your youtube URL here: ")
+yt = YouTube(link)
+videoservice = yt.streams.all()
+
+# Now it might be useful to find some way of numerating or showing the potential list of videos and video formats?
+
+video = list(enumerate(videoservice))
+for i in video:
+    print(i)
+
+print("Please select the format")
+option = int(input("Enter your option: "))
+video_option = videoservice[option]
+video_option.download()
+
+# Classy closing statement
+
+print("Your download has been successfully completed")
